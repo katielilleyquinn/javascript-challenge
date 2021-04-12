ef99c0d8bca52d74420a8c138a6775bcfad576c7
katielilleyquinn <75915238+katielilleyquinn@users.noreply.github.com>
2021-04-12 17:59:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -36,20 +36,3 @@ button.on("click", function() {
 
     });
 });
-
-
-// var button = d3.select("#datetime");
-// var InputValue = inputElement.property("value");
-// console.log(inputValue);
-// var newData = tableData.filter(sighting => sighting.datetime === inputValue);
-// console.log(newData);
-
-// newData.forEach(function(selections) {
-
-//     console.log(selections);
-//     var row = tbody.append("tr");
-//     Object.entries(selections).forEach(function() {[key,value]});
-//         console.log(key,value);
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
